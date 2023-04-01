a7c6056b43e8081f54cf58f079818588d055c95e
gholtzap <gholtzap@asu.edu>
2023-04-01 09:58:14 -0700

    styles

--- src/App.js
+++ src/App.js
@@ -1,65 +1,125 @@
-import logo from './logo.svg';
 import './App.css';
-import { initializeApp } from 'firebase/app';
-import { getDatabase, ref, set, onValue } from "firebase/database"
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth"
-import { useEffect, useState, useRef } from 'react';
 
-const apiKey = process.env.API_KEY;
-const apikey = "AIzaSyCQ3CIsOYfCs_UgPYz45iX5nt6Rqb6-Xuo"
+import { useEffect, useState, useRef } from 'react';
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, set, onValue } from "firebase/database";
+import { getAuth, signInWithPopup, GoogleAuthProvider, setPersistence, browserLocalPersistence } from "firebase/auth";
+
+const API_KEY = process.env.REACT_APP_API_KEY
+const AUTH_DOMAIN = process.env.REACT_APP_AUTH_DOMAIN
+const PROJECT_ID = process.env.REACT_APP_PROJECT_ID
+const STORAGE_BUCKET = process.env.REACT_APP_STORAGE_BUCKET
+const MESSAGE_SENDER_ID = process.env.REACT_APP_MESSAGE_SENDER_ID
+const APP_ID = process.env.REACT_APP_ID
+const MEASUREMENT_ID = process.env.REACT_APP_MEASUREMENT_ID
+
 
 const firebaseConfig = {
-  apiKey: apikey,
-  authDomain: "chat-app-58284.firebaseapp.com",
-  databaseURL: "https://chat-app-58284-default-rtdb.firebaseio.com",
-  projectId: "chat-app-58284",
-  storageBucket: "chat-app-58284.appspot.com",
-  messagingSenderId: "141649007276",
-  appId: "1:141649007276:web:da71ab3443c0df223a4509",
-  measurementId: "G-5WLSE2TR71"
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGE_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
+const database = getDatabase(app)
 const messagesRef = ref(database, 'messages');
-
 
 function App() {
 
   const [messages, setMessages] = useState([]);
-  const [isSignedIn, setIsSignedIn] = useState(false);
-  const [userData, setUserData] = useState([]);
-
+  const [isSignedIn, setIsSignedIn] = useState(false)
+  const [userData, setUserData] = useState([])
+  const inputRef = useRef(null);
   const auth = getAuth(app);
 
-  useEffect(()=>{
-    if(auth.currentUser != null){
-      onValue(messagesRef, (snapshot) =>{
+  useEffect(() => {
+
+    if (auth.currentUser != null) {
+      onValue(messagesRef, (snapshot) => {
         setMessages(snapshot.val())
       })
     }
-  },[isSignedIn]) 
+
+  }, [isSignedIn])
 
   const signInWithGoogle = () => {
-    signInWithPopup(auth, new GoogleAuthProvider())
-      .then((result)=>{
-        const user = result.user
-        setUserData(user)
-        setIsSignedIn(true)
+    setPersistence(auth, browserLocalPersistence).then(() => {
+      return signInWithPopup(auth, new GoogleAuthProvider())
+        .then((result) => {
+          const user = result.user;
+          setUserData(user)
+          setIsSignedIn(true)
+          return;
+        }).catch((error) => {
+          alert(error.message)
+          return;
+        });
+    })
+  }
 
-      }).catch((error) => {
-        console.log(error.message)
-      });
+  const handleClick = () => {
+    let messageValue = inputRef.current.value
+    let newMessages = [...messages]
+    newMessages.push({
+      name: auth.currentUser.displayName,
+      message: messageValue
+    })
+    console.log(userData)
+    set(messagesRef, newMessages).then(() => {
+      inputRef.current.value = ""
+    })
   }
 
   return (
-    <div className="App">
-        <h1>chat app</h1>
-        {isSignedIn && <h2>Welcome {userData.displayName}!</h2>}
-        {!isSignedIn && <h2>You are not signed in!</h2>}
-        <button onClick = {signInWithGoogle}> Sign in with Google</button>
+    <div>
+      <center>
+        <h1>Chat app</h1>
+      </center>
+      {isSignedIn && (
+        <div className='messages'>
+          <ul>
+            {messages.map((message) => {
+              if (auth.currentUser != null) {
+                if (message.name == auth.currentUser.displayName) {
+                  return (
+                    <div className='message bluebubble'>
+                      <div className='name'>{message.name}</div>
+                      <div className='bubble'>{message.message}</div>
+                    </div>
+                  )
+                } else {
+                  return (
+                    <div className='message'>
+                      <div className='name'>{message.name}</div>
+                      <div className='bubble'>{message.message}</div>
+                    </div>
+                  )
+                }
+              }
+            })}
+          </ul>
+          <center>
+            <div className='sendbox'>
+              <input type="text" ref={inputRef} placeholder="Message"></input>
+              <button onClick={handleClick}>Send!</button>
+            </div>
+          </center>
+        </div>
+      )}
+      {!isSignedIn &&
+        <center>
+          <button class={"signin"} onClick={signInWithGoogle}>Sign in with Google</button>
+        </center>
+      }
     </div>
   );
+
+
 }
 
 export default App;
