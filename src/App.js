4e85578fcba11e814d9aaf2b5d3de5561a8af30f
gholtzap <gholtzap@asu.edu>
2023-03-31 19:05:43 -0700

    init

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth"
 import { useEffect, useState, useRef } from 'react';
 
 const apiKey = process.env.API_KEY;
+const apikey = "AIzaSyCQ3CIsOYfCs_UgPYz45iX5nt6Rqb6-Xuo"
 
 const firebaseConfig = {
-  apiKey: apiKey,
+  apiKey: apikey,
   authDomain: "chat-app-58284.firebaseapp.com",
   databaseURL: "https://chat-app-58284-default-rtdb.firebaseio.com",
   projectId: "chat-app-58284",
@@ -19,10 +20,9 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const database = getDatabase(app);
-
 const messagesRef = ref(database, 'messages');
+
 
 function App() {
 
@@ -32,9 +32,32 @@ function App() {
 
   const auth = getAuth(app);
 
+  useEffect(()=>{
+    if(auth.currentUser != null){
+      onValue(messagesRef, (snapshot) =>{
+        setMessages(snapshot.val())
+      })
+    }
+  },[isSignedIn]) 
+
+  const signInWithGoogle = () => {
+    signInWithPopup(auth, new GoogleAuthProvider())
+      .then((result)=>{
+        const user = result.user
+        setUserData(user)
+        setIsSignedIn(true)
+
+      }).catch((error) => {
+        console.log(error.message)
+      });
+  }
+
   return (
     <div className="App">
         <h1>chat app</h1>
+        {isSignedIn && <h2>Welcome {userData.displayName}!</h2>}
+        {!isSignedIn && <h2>You are not signed in!</h2>}
+        <button onClick = {signInWithGoogle}> Sign in with Google</button>
     </div>
   );
 }
